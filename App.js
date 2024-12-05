7eeac3c079e85199022a059a5494183fb86476f2
Comfort Ngwenya <>
2024-12-05 22:10:27 +0200

    Wrapping up the app with the redux Provider

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import AppNavigator from './navigation/AppNavigation';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
 
