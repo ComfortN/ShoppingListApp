79e60ddac8ebd51d3e0d0ca0be9859484d7ce208
Comfort Ngwenya <>
2024-12-06 09:10:39 +0200

    Adding the colors for the app style

---
+++ src/theme/colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+    background: '#DFF0F1',
+    primary: '#233B5D',
+    secondary: '#A1ADBA',
+    text: '#6A8396',
+    border: '#DBEBEE',
+  };
+  
+  export default Colors;
+  
